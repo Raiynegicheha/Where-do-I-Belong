bcc3c1c555ad4e9980fc6dd507032a34183ac2a1
Ruth Gicheha <60509305+Raiynegicheha@users.noreply.github.com>
2023-06-24 23:31:47 +0300

    Update sort.js

--- sort.js
+++ sort.js
@@ -1,12 +1,14 @@
 function getIndexToIns(arr, num) {
 
   arr.sort((a, b) => a - b);
-
+// here the array is sorted from the lowest to the largest using the .sort(callbackFunction).
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] >= num) return i;
   }
 
   return arr.length;
 }
+//and thn we run a for loop throught the array to compare items in the array starting from the smallest one. 
+//When an item on the array is greater than the number we are comparing against, then we return the index.
 
 getIndexToIns([40, 60], 50);
