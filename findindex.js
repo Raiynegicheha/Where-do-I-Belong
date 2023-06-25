9819eb893b55387dc955144f22fec8fc36ef91c2
Ruth Gicheha <60509305+Raiynegicheha@users.noreply.github.com>
2023-06-26 00:00:13 +0300

    Update findindex.js

--- findindex.js
+++ findindex.js
@@ -8,3 +8,11 @@ function getIndexToIns(arr, num) {
 }
 
 getIndexToIns([40, 60], 500);
+
+/*First sort the array in ascending order, this is currently done using array functions for minimal footprint.
+Once the array is sorted, we directly apply the .findIndex() where we are going to compare every element in the array 
+until we find where num <= currNum - where the number we want to insert is less than or equal to the current number in the iteration.
+Then we use ternary operations to check whether we got an index returned or -1. 
+We only get -1 when the index was not found, meaning when we get a false for all elements in the array, 
+and for such case, it would mean that num should be inserted at the end of the list. 
+Hence, why we use arr.length.*/
